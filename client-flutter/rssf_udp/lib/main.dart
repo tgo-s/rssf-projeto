f435e4787e64a535b6cc59709c4fcd0c488f71fb
Thiago Simionato dos Santos <thiago.santos@sanepar.com.br>
2018-11-23 14:12:14 -0200

    UI funcionando

--- client-flutter/rssf_udp/lib/main.dart
+++ client-flutter/rssf_udp/lib/main.dart
@@ -2,20 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:rssf_udp/core/udp_rssf.dart';
 import 'dart:io';
 
-void main() async {
-  FlutterUdpClient fClient = FlutterUdpClient();
-  final String _serverAddr = "10.0.2.2";
-  final int _serverPort = 8802;
-  bool connected = await fClient.startClient(_serverAddr, _serverPort);
-  if (connected) {
-    runApp(RssfApp(fClient));
-  }
-}
+void main() => runApp(RssfApp());
 
 class RssfApp extends StatelessWidget {
-  final FlutterUdpClient flutterUdpClient;
-
-  RssfApp(this.flutterUdpClient);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -25,18 +14,14 @@ class RssfApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RssfHomePage(
-        title: 'CEIOT - Projeto RSSF ',
-        channel: flutterUdpClient,
-      ),
+      home: RssfHomePage(title: 'CEIOT - Projeto RSSF '),
     );
   }
 }
 
 class RssfHomePage extends StatefulWidget {
-  RssfHomePage({Key key, this.title, @required this.channel}) : super(key: key);
+  RssfHomePage({Key key, this.title}) : super(key: key);
   final String title;
-  final FlutterUdpClient channel;
 
   @override
   _RssfHomePageState createState() => _RssfHomePageState();
@@ -45,39 +30,19 @@ class RssfHomePage extends StatefulWidget {
 class _RssfHomePageState extends State<RssfHomePage> {
   bool _ledIsOn;
   int _lightIntesity;
-  String _debugMsg;
   final int _defaultIntensityValue = 10;
-  final String _serverAddr = "10.0.2.2";
-  final int _serverPort = 8802;
-
-  Future<List<int>> sendPackage(int op, int value) async {
-    List<int> package = [];
-
-    package = await widget.channel.sendPackage(_serverAddr, _serverPort, op, 1);
-
-    return package;
-  }
 
   @override
   void initState() {
-    _debugMsg = "Client connected";
-    _debugMsg = "Getting led status";
-    sendPackage(Operation.LED_GET_STATE, 1).then((result) {
-      List<int> package = result;
-      if (package.length > 0 && package[0] == Operation.LED_STATE) {
-        _lightIntesity = package[1];
-        if (_lightIntesity > 0) {
-          _ledIsOn = true;
-        }
-      }
+    setState(() {
+      _ledIsOn = false;
+      _lightIntesity = 0;
     });
-
     super.initState();
   }
 
   @override
   void dispose() {
-    widget.channel.closeClient();
     super.dispose();
   }
 
@@ -104,26 +69,11 @@ class _RssfHomePageState extends State<RssfHomePage> {
                       child: Text("Light Down"),
                       onPressed: () {
                         setState(() {
-                          List<int> localPackage = [];
-                          _debugMsg = "Sending Light Down package";
-                          sendPackage(
-                                  Operation.LIGHT_DOWN, _defaultIntensityValue)
-                              .then((result) {
-                            _debugMsg = "Package received";
-                            localPackage = result;
-                            if (localPackage.length > 0 &&
-                                localPackage[0] == Operation.SUCCESS &&
-                                localPackage[1] == 1) {
-                              _debugMsg = "Success";
-                              _lightIntesity -= _defaultIntensityValue;
-                              if (_lightIntesity <= 0) {
-                                _ledIsOn = false;
-                                _lightIntesity = 0;
-                              }
-                            } else {
-                              _debugMsg = "Error";
-                            }
-                          });
+                          _lightIntesity -= _defaultIntensityValue;
+                          if (_lightIntesity <= 0) {
+                            _ledIsOn = false;
+                            _lightIntesity = 0;
+                          }
                         });
                       },
                     ),
@@ -131,26 +81,11 @@ class _RssfHomePageState extends State<RssfHomePage> {
                       child: Text("Light Up"),
                       onPressed: () {
                         setState(() {
-                          List<int> localPackage = [];
-                          _debugMsg = "Sending Light Down package";
-                          sendPackage(
-                                  Operation.LIGHT_UP, _defaultIntensityValue)
-                              .then((result) {
-                            localPackage = result;
-                            _debugMsg = "Package received";
-                            if (localPackage.length > 0 &&
-                                localPackage[0] == Operation.SUCCESS &&
-                                localPackage[1] == 1) {
-                              _debugMsg = "Success";
-                              _lightIntesity += _defaultIntensityValue;
-                              _ledIsOn = true;
-                              if (_lightIntesity >= 100) {
-                                _lightIntesity = 100;
-                              }
-                            } else {
-                              _debugMsg = "Error";
-                            }
-                          });
+                          _lightIntesity += _defaultIntensityValue;
+                          _ledIsOn = true;
+                          if (_lightIntesity >= 100) {
+                            _lightIntesity = 100;
+                          }
                         });
                       },
                     )
@@ -161,13 +96,6 @@ class _RssfHomePageState extends State<RssfHomePage> {
                 margin: EdgeInsets.only(top: 10.0),
                 child: Text("Led intensity is at: $_lightIntesity"),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 30.0),
-                child: Text(
-                  _debugMsg,
-                  style: TextStyle(color: Colors.lightGreen),
-                ),
-              )
             ],
           ),
         ),
